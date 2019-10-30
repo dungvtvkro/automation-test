20754650343b8ef92978949c0e4b598684ac6049
waichuan <nwai1990@hotmail.com>
2019-10-30 16:51:22 +0800

    Set up env

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ API documentation at https://taiko.gauge.org
 
 # Running tests
 
-- `npm test` to run all specs
+- `npm test` or `gauge run specs` to run all specs for QA site
+- `gauge run specs -e prod` to run all specs for live site
 - `gauge run specs/SPEC_NAME.js` to run a single spec
--- tests/auth.js
+++ tests/auth.js
@@ -1,10 +1,10 @@
 /* globals gauge*/
 "use strict";
-const { openBrowser, closeBrowser, goto, text, click, into, write, textBox, button, image, waitFor, $} = require('taiko');
+const { openBrowser, closeBrowser, goto, text, click, into, write, textBox, button, image, waitFor, $ } = require('taiko');
 const assert = require("assert");
+const env = process.env.NODE_ENV || 'default';
 const headless = process.env.headless_chrome.toLowerCase() === 'true';
-const config = require('../env/config.json');
-const env = process.env.NODE_ENV || 'development';
+const config = require(`../env/${env}/config.json`);
 
 beforeSuite(async () => {
     await openBrowser({ headless: headless })
@@ -15,10 +15,10 @@ afterSuite(async () => {
 });
 
 step("Log in as <user_type>", async (user_type) => {
-    const login_email = user_type == "personal" ? config[env]["personal_user_email"] : config[env]["business_user_email"];
-    const login_password = user_type == "personal" ? config[env]["personal_user_password"] : config[env]["business_user_password"];
+    const login_email = user_type == "personal" ? config["personal_user_email"] : config["business_user_email"];
+    const login_password = user_type == "personal" ? config["personal_user_password"] : config["business_user_password"];
 
-    await goto(config[env]["site"]);
+    await goto(config["site"]);
     await click('Log in');
     await write(login_email, into(textBox({ name: 'email' })));
     await write(login_password, into(textBox({ name: 'password' })));
@@ -28,7 +28,7 @@ step("Log in as <user_type>", async (user_type) => {
 
 step("Log out", async () => {
     await waitFor(3000); // wait for covering frame to disappear
-    await click(image({class:'pull-left ng-isolate-scope'}));
+    await click(image({ class: 'pull-left ng-isolate-scope' }));
     await click('Logout');
     assert.ok(await text('Log in').exists());
 });
